6dd1cc656483d0d9cf4040bdde29388c5f2336c6
ozgun <selim.ozgun.0@gmail.com>
2023-04-05 12:12:09 +0300

    map yeniden tasarlandi level desig, karakter movement

---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
